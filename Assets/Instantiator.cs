8ef978dc482f916beff9d46d3f5ebcf57fcc7df6
CaroKaufman <101335676+CaroKaufman@users.noreply.github.com>
2022-05-09 14:26:22 -0300

    Initial commit

---
+++ Assets/Instantiator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject objectToClone;
+    public int CantVeces;
+    
+
+    public void CloneObject()
+    {
+        int counter = 10;
+        while (CantVeces<=counter){
+            Instantiate(objectToClone);
+            counter --;
+        }
+
+            
+       
+    }
+}
